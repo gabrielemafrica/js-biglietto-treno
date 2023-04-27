5f54b4ddf9abedda0925f6775eea9890c82259c7
gabrielemafrica <gabriele.ga@live.it>
2023-04-27 18:09:37 +0300

    finale

---
+++ js/myjs.js
@@ -0,0 +1,85 @@
+//presentazione
+alert("Benvenuto al colcolatore di prezzo, INSERISCI I DATI RICHIESTI.")
+//crea variabili globali
+
+let userKm;
+let userAge;
+let discountUnder;
+let discountOver;
+let price;
+let priceDiscount;
+let messageKm;
+let messageEta;
+let messageDiscount;
+let messageSconto;
+let messagePrezzo;
+let messageFinalPrice;
+
+
+//chiedi chilometri
+
+userKm = parseInt(prompt("Quanti chilometri devi percorrere?"));
+userAge = parseInt(prompt("Quanti anni hai?"));
+
+
+//calcolo prezzo intero
+
+price = userKm * 0.21;
+
+
+console.log("km = ", userKm);
+console.log("eta = ", userAge);
+console.log("prezzo = ", price);
+
+
+//calcolo valore sconto
+
+discountUnder = price * 20 / 100 ;
+discountOver = price * 40 / 100 ;
+
+console.log("sconto minorenni = ", discountUnder);
+console.log("sconto vecchi = ", discountOver);
+
+// calcolo prezzo scontato
+
+if(userAge < 18){ //under
+    priceDiscount = parseFloat((price - discountUnder).toFixed(2));
+    //messaggi sconto
+    messageDiscount = `Hai diritto allo sconto under18 pari al 20%`;
+    messageSconto = `Applicazine sconto: ${price} Euro - ${discountUnder} Euro`;
+    
+    
+}else if(userAge >= 65){ //over
+    priceDiscount = parseFloat((price - discountOver).toFixed(2));
+    //messaggi sconto
+    messageDiscount = `Hai diritto allo sconto over65 pari al 40%`;
+    messageSconto = `Applicazine sconto: ${price} Euro - ${discountOver} Euro`;
+   
+    
+}else{ //no sconto
+    priceDiscount = parseFloat(price.toFixed(2));
+    //messaggi sconto
+    messageDiscount = `Non hai diritto a sconti.`;
+    messageSconto = `Applicazine sconto: ${price} Euro - 0 Euro`;
+    
+}
+
+console.log("prezzo scontato giovani = ", priceDiscount);
+console.log("prezzo scontato vecchi = ", priceDiscount);
+
+//creazione messaggi
+
+messageKm = `Devi percorrere ${userKm} km`;
+messageEta = `Hai ${userAge} anni`;
+messagePrezzo = `Prezzo intero ${price} Euro`;
+messageFinalPrice = `Il prezzo finale é ${priceDiscount} Euro`;
+
+
+//output
+
+document.getElementById("chilometri").innerHTML = messageKm;
+document.getElementById("eta").innerHTML = messageEta;
+document.getElementById("scontistica").innerHTML = messageDiscount;
+document.getElementById("prezzo").innerHTML = messagePrezzo;
+document.getElementById("sconto").innerHTML = messageSconto;
+document.getElementById("finalPrice").innerHTML = messageFinalPrice;
